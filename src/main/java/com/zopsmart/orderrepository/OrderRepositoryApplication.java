50cc6f1c014eaa23d09ae9e4bbfc81ebfb8cce66
Bajrang Prasad <bajrang.prasad01@gmail.com>
2020-05-20 13:05:52 +0530

    Initial commit

---
+++ src/main/java/com/zopsmart/orderrepository/OrderRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.zopsmart.orderrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zopsmart/orderrepository/OrderRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zopsmart.orderrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
